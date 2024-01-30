0bb99c41e433c0b4d9c42c01985c0023cc6360a9
lvsouza <ls18017@gmail.com>
2024-01-30 12:44:06 -0300

    feat: Add numeric format

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { NumericFormat } from 'react-number-format'
+
+
+export default function App() {
+  const [value, setValue] = useState('')
+
+
+  return (
+    <>
+      {value}
+
+      <NumericFormat
+        value={value}
+        onValueChange={(values) => { setValue(values.value); console.log(values) }}
+
+        allowLeadingZeros={false}
+        allowNegative={false}
+
+        decimalScale={2}
+        fixedDecimalScale={true}
+        decimalSeparator=','
+        allowedDecimalSeparators={['.']}
+
+        prefix='R$ '
+        //suffix='$'
+        thousandSeparator='.'
+        //thousandsGroupStyle='thousand'
+      />
+    </>
+  )
+}
